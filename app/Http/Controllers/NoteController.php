bdd72f54b24e1b9c154ddc0eeeded602e783b7db
Funki7809 <vegahector.medina@gmail.com>
2024-05-20 15:45:33 -0600

    add file class

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Author;
 use App\Models\Category;
 use App\Models\Picture;
+use Illuminate\Support\Facades\File;
 
 class NoteController extends Controller
 {
